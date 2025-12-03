c50db3156e0570a43a3f4babc666a261394d9827
gatogordo3 <percksonrony@gmail.com>
2025-12-03 18:19:58 -0300

    add main.c
    god what am i doing with my life

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+ //botom 1 code oat
+int tuff = 0;
+printf("#####didy blud tuff calc v67###### \n");
+printf("enter number to calculate you didy blud: ");
+ scanf("%d" , &tuff);
+  if (tuff == 67) {
+ for (int i = 1; i <= 67; i++){
+printf("NUMBER TUFFNESS: 67%% AYOOOOOO SO TUFF🥭🥭🥭 THOSE WHO KNOW💀💀💀💀 \n");
+} } else {
+printf("idk vro maybe its tuff \n");
+}
+return 0;
+}
